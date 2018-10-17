87d26cdf255dea6a80d57c9d9a05c78bff5dc699
odesenvolvedor <fernando@odesenvolvedor.net>
2018-10-17 11:42:46 -0300

    Classe TestaLista

--- src/java/br/com/javaweb/jdbc/dao/ContatoDao.java
+++ src/java/br/com/javaweb/jdbc/dao/ContatoDao.java
@@ -5,7 +5,11 @@ import br.com.javaweb.jdbc.modelo.Contato;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ContatoDao {
 
@@ -26,10 +30,43 @@ public class ContatoDao {
             stmt.setString(1, contato.getNome());
             stmt.setString(2, contato.getEmail());
             stmt.setString(3, contato.getEndereco());
-            stmt.setDate(4, new Date( contato.getDataNascimento().getTimeInMillis()) );
+            stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
             //	executa
             stmt.execute();
             stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Contato> getLista() {
+        try {
+            List<Contato> contatos = new ArrayList<Contato>();
+            PreparedStatement stmt = this.connection.prepareStatement( "select * from contatos");
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                // criando o objeto Contato    
+                Contato contato
+                        = new Contato();
+                contato.setId(rs.getLong("id"));
+                contato.setNome(rs.getString("nome"));
+                contato.setEmail(rs.getString("email"));
+                contato.setEndereco(rs.getString("endereco"));
+
+                // montando a data através do Calendar
+                Calendar data = Calendar.getInstance();
+                data.setTime(rs.getDate("dataNascimento"));
+                contato.setDataNascimento(data);
+                
+                // adicionando o objeto à lista
+                contatos.add(contato);
+            }
+            rs.close();
+            stmt.close();
+            return contatos;
+            
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
---
+++ src/java/br/com/javaweb/jdbc/test/TestaLista.java
@@ -0,0 +1,37 @@
+package br.com.javaweb.jdbc.test;
+
+import br.com.javaweb.jdbc.dao.ContatoDao;
+import br.com.javaweb.jdbc.modelo.Contato;
+import java.util.List;
+
+public class TestaLista {
+
+    public static void main(String[] args) {
+        ContatoDao contatoDao = new ContatoDao();
+
+        List<Contato> contatos = contatoDao.getLista();
+        
+        TestaLista test = new TestaLista();
+        //test.iteraNormal(contatos);
+        test.iteraFuncional(contatos);
+    }
+    
+    private void iteraNormal(List<Contato> contatos) {
+        for (Contato contato : contatos) {
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Email: " + contato.getEmail());
+            System.out.println("Endereço: " + contato.getEndereco());
+            System.out.println("Data de Nascimento: " + contato.getDataNascimento().getTime() + "\n");            
+        }        
+    }
+    
+    private void iteraFuncional(List<Contato> contatos) {
+        contatos.forEach((contato) -> {
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Email: " + contato.getEmail());
+            System.out.println("Endereço: " + contato.getEndereco());
+            System.out.println("Data de Nascimento: " + contato.getDataNascimento().getTime() + "\n");            
+        });
+    }
+    
+}
