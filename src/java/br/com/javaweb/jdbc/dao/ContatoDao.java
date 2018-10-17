ed400620a97b51a8294b6c6e9546bbf9575ef62d
odesenvolvedor <fernando@odesenvolvedor.net>
2018-10-17 11:57:55 -0300

    Novos metodos no ContatoDao

--- src/java/br/com/javaweb/jdbc/dao/ContatoDao.java
+++ src/java/br/com/javaweb/jdbc/dao/ContatoDao.java
@@ -39,13 +39,43 @@ public class ContatoDao {
         }
     }
 
+    public void altera(Contato contato) {
+        String sql
+                = "update contatos set nome=?, email=?,"
+                + "endereco=?, dataNascimento=? where id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, contato.getNome());
+            stmt.setString(2, contato.getEmail());
+            stmt.setString(3, contato.getEndereco());
+            stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+            stmt.setLong(5, contato.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void remove(Contato contato) {
+        try {
+            String sql = "delete from contatos where id=?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, contato.getId());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public List<Contato> getLista() {
         try {
             List<Contato> contatos = new ArrayList<Contato>();
-            PreparedStatement stmt = this.connection.prepareStatement( "select * from contatos");
-            
+            PreparedStatement stmt = this.connection.prepareStatement("select * from contatos");
+
             ResultSet rs = stmt.executeQuery();
-            
+
             while (rs.next()) {
                 // criando o objeto Contato    
                 Contato contato
@@ -59,14 +89,14 @@ public class ContatoDao {
                 Calendar data = Calendar.getInstance();
                 data.setTime(rs.getDate("dataNascimento"));
                 contato.setDataNascimento(data);
-                
+
                 // adicionando o objeto à lista
                 contatos.add(contato);
             }
             rs.close();
             stmt.close();
             return contatos;
-            
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
